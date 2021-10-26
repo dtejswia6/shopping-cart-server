706429255b08669eba96c3e8f154ebf101ee112f
Tejaswi <tejaswi9sharma@gmail.com>
2021-10-26 19:14:51 +0530

    'changes'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://mongodb:mongodb@cluster0.clb4t.mongodb.net/shoppingCart?retryWrites=true&w=majority'), ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
+
---
+++ src/products/schemas/product.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ProductDocument = Product & Document;
+
+@Schema()
+export class Product {
+  @Prop()
+  productName: string;
+
+  @Prop()
+  productQuantity: number;
+
+  @Prop()
+  productDescription: string;
+
+  @Prop()
+  productPrice: number;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
