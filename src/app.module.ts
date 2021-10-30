634ac29e401c8fc90ca26c2fb1fe81546f005e45
Tejaswi <tejaswi9sharma@gmail.com>
2021-10-30 08:31:33 +0530

    'changes'

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://mongodb:mongodb@cluster0.clb4t.mongodb.net/shoppingCart?retryWrites=true&w=majority'), ProductsModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://mongodb:mongodb@cluster0.clb4t.mongodb.net/shoppingCart?retryWrites=true&w=majority'), ProductsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
